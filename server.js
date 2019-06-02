8156e52de0f71a9727e0f594dfbc3be74e8165a2
Fuzzy Hair <pamelasanchezhernandez@Pamelas-MBP.lan1>
2019-06-02 11:06:10 -0400

    fix folder set up MVC

--- server.js
+++ server.js
@@ -1,30 +1,55 @@
 var express = require("express");
-var logger = require("morgan");
 var mongoose = require("mongoose");
+var expressHandelbars = require("express-handlebars");
+var bodyParser = require("body-parser");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
 
-// Require all models
-var db = require("./models");
-
-var PORT = 3000;
+// Set up port to be either the host's designated port, or 3000
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
 
-// Configure middleware
+// Set up Express Router
+var router = express.Router();
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+// Connect handelbars to the express app
+app.engine("handlebars", expressHandelbars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Use bodyParser in the app
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+
+// Configure middleware
+// Have every request go through our router middleware
+app.use(router);
+
+// If deployed, use the deployed database. Otherwise use the local MongHeadLines database
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLines";
+
+// Connect mongoose to our datatbase
+mongoose.connect(db, function(error) {
+    // log any errors connecting with mongoose
+    if (error) {
+        console.log(error);
+    }
+    // Or log a success message
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
+
+
+// Listen on port
+app.listen(PORT, function() {
+    console.log("listening on port:" + PORT)
+});
